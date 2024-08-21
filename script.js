25ed505950b1d88d411d2a31764e9746c0e2a830
Beatriz Correia De Araujo <108531051+beatrizcorreiaaraujo@users.noreply.github.com>
2024-08-21 12:24:50 -0300

    S

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPrincipal = document.querySelector(".caixa-perguntas")
+const caixaPrincipal = document.querySelector(".caixa-alternativas")
+const caixaPrincipal = document.querySelector(".caixa-resultado")
+const caixaPrincipal = document.querySelector(".texto-resultado")
